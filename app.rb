a15fdf83b7c42f27183a25c42ea14aa43a1745f3
Nicolas Marini <nmarini44@gmail.com>
2018-11-17 19:22:10 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,7 +28,7 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
 
-    @answer = nil
+    # @answer = nil
     if params[:operation] == "add"
       @answer = (@num1 + @num2).to_s
     elsif params[:operation] == "subtract"
